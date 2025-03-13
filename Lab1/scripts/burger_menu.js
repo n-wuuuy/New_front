f346e2e75ab4f3ab9fb5540ba04a7ba8122334d9
Никита Дубойский <n.duboisky@gmail.com>
2025-03-13 17:31:40 +0300

    init

---
+++ Lab1/scripts/burger_menu.js
@@ -0,0 +1,6 @@
+const burgerMenu = document.getElementById('burger-menu');
+const navLinks = document.getElementById('nav-links');
+
+burgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
